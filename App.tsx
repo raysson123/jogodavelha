941dc57d04490ef55e319dc5a54435e4f91b9236
raysson123 <raysson123@gmail.com>
2023-01-10 17:55:23 -0300

    verção 1

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+  const [tela, setTela] = useState('menu');
+  switch (tela){
+    case 'menu':
+      return getTelaMenu();
+  }
+  function  getTelaMenu(){
+    return (
+        <View style={styles.container}>
+          <Text>meunu</Text>
+          <StatusBar style="auto" />
+        </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
